c8618f133d12205bec545b53c09727d87e7c7c04
Adham-Magdy <adhammagdy349@gmail.com>
2025-04-28 12:35:45 +0300

    feat:  enhanced property api Media

--- src/app/components/properties/PropertyListItem.tsx
+++ src/app/components/properties/PropertyListItem.tsx
@@ -11,7 +11,7 @@ const PropertyListItem:React.FC<IPropertyProps> = ({property}) => {
     <div className="relative overflow-hidden aspect-square rounded-xl">
       <Image
       fill
-      src={"/beach_1.jpg"}
+      src={property.image_url}
       alt="Beach Property"
       sizes="(max-width: 768px) 768px, (max-width: 1200px) 768px , 780px"
       className='hover:scale-110 object-cover transition h-full w-full'
