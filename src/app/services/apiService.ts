033aa28a0d485c15eded2efa460bdddc5697dae3
Adham-Magdy <adhammagdy349@gmail.com>
2025-04-28 13:57:19 +0300

    feat: implement post method for  api service

--- src/app/services/apiService.ts
+++ src/app/services/apiService.ts
@@ -1,22 +1,45 @@
 const apiService = {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    get: async function(url:string): Promise<any>{
-        return new Promise((resolve,reject)=>{
-            fetch(url,{
-                method:'GET',
-                headers:{
-                    'ACCEPT':'application/json',
-                    'Content-Type':'application/json'
-                }
-            }).then((response)=> response.json())
-            .then((json)=>{
-                console.log('Response:',json)
-                resolve(json)
-        }).then((error)=>{
-            reject(error)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  get: async function (url: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "GET",
+        headers: {
+          ACCEPT: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log("Response:", json);
+          resolve(json);
         })
+        .then((error) => {
+          reject(error);
+        });
+    });
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  post: async function (url: string, data: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        body: data,
+        headers: {
+          ACCEPT: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log("Response: ", json);
+          resolve(json);
         })
-    }
-}
+        .then((error) => {
+          reject(error);
+        });
+    });
+  },
+};
 
 export default apiService;
