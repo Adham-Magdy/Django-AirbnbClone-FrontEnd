f080cafb2207f91cf3fbad1472744e50b7670751
Adham-Magdy <adhammagdy349@gmail.com>
2025-05-02 16:01:14 +0300

    handle add property service to be interactive with user choices

--- src/app/components/modals/AddPropertyModel.tsx
+++ src/app/components/modals/AddPropertyModel.tsx
@@ -1,22 +1,39 @@
-'use client';
-import usePropertyModel from '@/app/hooks/usePropertyModel';
-import React from 'react'
-import Modals from './Modals';
+"use client";
+import usePropertyModel from "@/app/hooks/usePropertyModel";
+import React, { useState } from "react";
+import Modals from "./Modals";
+import CustomButton from "../form/CustomButton";
 
 const AddPropertyModel = () => {
+  const addPropertyModel = usePropertyModel();
+  const [currentStep, setCurrentStep] = useState(1);
 
-    const addPropertyModel = usePropertyModel();
+  const content = (
+    <>
+      {currentStep == 1 ? (
+        <>
+          <h2 className="mb-6 text-2xl">Choose Category</h2>
+          <CustomButton label="Next" onClick={() => setCurrentStep(2)} />
+        </>
+      ) : (
+        <>
+          <p>Step 2</p>
+        </>
+      )}
+    </>
+  );
+  console.log(currentStep);
+
   return (
     <>
       <Modals
-      isOpen={addPropertyModel.isOpen}
-      close={addPropertyModel.close}
-      label='Add Property'
-      content={<div>Yoo</div>}
-      
+        isOpen={addPropertyModel.isOpen}
+        close={addPropertyModel.close}
+        label="Add Property"
+        content={content}
       />
     </>
-  )
-}
+  );
+};
 
-export default AddPropertyModel
+export default AddPropertyModel;
